d8b93d38208e41420c45df570b98190ba52feaed
Alexander <mcmillancontact@protonmail.com>
2023-09-05 14:53:04 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+
+
+
+
+const cards = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(cards) {
+  const messages = []
+  for (let i = 0; i < cards.length; i++) {
+        console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+        messages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+
+    }
+
+
+
+    return messages;
+}
+
+writeCards(cards);
+
+
+function countDown() {
+  let countdown = 10;
+  while (countdown > -1) {
+    console.log(countdown--)
+  }
+}
